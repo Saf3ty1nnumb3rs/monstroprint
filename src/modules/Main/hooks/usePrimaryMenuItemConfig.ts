9bdd6c8ab092217f06beaa8d4f3532e6863cd78c
Joshua Sample <josh.sample@ware2go.com>
2020-12-01 17:16:59 -0500

    Quick text updTE TO menu item config

--- src/modules/Main/hooks/usePrimaryMenuItemConfig.ts
+++ src/modules/Main/hooks/usePrimaryMenuItemConfig.ts
@@ -3,35 +3,35 @@ import { SelectablePrimaryMenuItem } from 'library/types/BaseComponentTypes';
 export const usePrimaryMenuItemConfig = (): SelectablePrimaryMenuItem[] => {
   const primaryMenuItems = [
     {
-      title: 'Sci Fi',
+      title: 'sci fi',
       subtitle: 'SHOP NOW',
       imageUrl:
         'https://drive.google.com/uc?id=13_m7CVDlUPYdkkWtFdkLXnziCiOqluHt',
       className: 'small',
     },
     {
-      title: 'Industrial',
+      title: 'industrial',
       subtitle: 'SHOP NOW',
       imageUrl:
         'https://drive.google.com/uc?id=1_gRxjg9iDiSQN3drtv4ZGzta2mpzglqo',
       className: 'small',
     },
     {
-      title: 'Historic',
+      title: 'historic',
       subtitle: 'SHOP NOW',
       imageUrl:
         'https://drive.google.com/uc?id=1FasurgWsETQxh-XrzAjg3Ks90rXz1Vas',
       className: 'small',
     },
     {
-      title: 'Imported',
+      title: 'imported',
       subtitle: 'SHOP NOW',
       imageUrl:
         'https://drive.google.com/uc?id=1dE1xYZUuWrbjidR-FkscxE_nARybPGgs',
       className: 'large',
     },
     {
-      title: 'Propaganda',
+      title: 'propaganda',
       subtitle: 'SHOP NOW',
       imageUrl:
         'https://drive.google.com/uc?id=1SvpNUlllPii8mj-dMYMLkIfV8ZBs3JON',
