cf76e13d25475fa25d6d6df1e4b4de54a2e61291
Joshua Sample <josh.sample@ware2go.com>
2020-12-05 09:11:56 -0500

    ShoppingCart - remoce unneeded values

--- src/library/components/CartIcon/CartIcon.tsx
+++ src/library/components/CartIcon/CartIcon.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from 'assets/shoppingBag.svg';
-import { useSelector } from 'react-redux';
-import { selectCartHidden } from 'redux/cart/cartSelectors';
 import { useCallCartDispatch } from 'library/hooks/useCallCartDispatch';
 
 import 'scss/components/_CartIcon.scss';
 
 const CartIcon = (): React.ReactElement => {
   const { toggleCartHidden } = useCallCartDispatch();
-  const isCartHidden = useSelector(selectCartHidden);
 
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
